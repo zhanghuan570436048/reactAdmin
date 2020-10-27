0525855514b741cce7938b0ced13ee9804a77ef1
zhanghuan570436048 <570436048@qq.com>
2020-10-28 07:48:19 +0800

    初始化页面

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import About from './view/About';
+import Home from './view/Home';
 import './App.css';
+import { Switch, Route, HashRouter} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<HashRouter>
+  <Switch>
+    <Route component={About} exact path="/about" />
+    <Route component={Home} path="/home" />
+  </Switch>
+</HashRouter>
+
   );
 }
 
